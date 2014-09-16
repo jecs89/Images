d1e5d1dab89c2a278d5468ebae6e57269b145e68
jecs89 <jecs89@gmail.com>
2014-09-16 14:15:34 -0500

    Histogram, Eq Histogram, Source, Destiny Completed

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <vector>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
@@ -11,23 +12,42 @@ int main(int argc, char** argv )
 {
     int histSize = 256;
 
-    //reading image
-    Mat image_src, histogram_graph( 700, 400, CV_8UC3, Scalar( 255,255,255) ), eq_histogram_graph( 700, 400, CV_8UC3, Scalar( 255,255,255) ), image_dest;
+    ofstream myfile;
+    myfile.open ("values.txt");
 
+    //Initializing mat
+    Mat image_src, histogram_graph( 700, 400, CV_8UC3, Scalar( 255,255,255) ), eq_histogram_graph( 700, 400, CV_8UC3, Scalar( 255,255,255) );
+
+    //reading image
     image_src = imread( argv[1], 0); // 0, grayscale  >0, color
-    image_dest = image_src;
+
+    imwrite("image_grayscale.jpg", image_src);
+
+    //dimensions of image
+    int image_width = image_src.cols;
+    int image_height = image_src.rows;
+
+    cout << image_src.rows << "\t" << image_src.cols << endl;
+    //cout << image_height   << "\t" << image_width    << endl;
+
+    //mat of destiny
+    //Mat image_dest( image_height, image_width, CV_8UC1, Scalar( 255,255,255) );
+    Mat image_dest = imread("image_grayscale.jpg", 0);
+
+    cout << image_dest.rows << "\t" << image_dest.cols << endl;
+    cout << "# pixels " << image_width * image_height << endl;
 
     //vector for histogram and eq_histogram
     vector<int> histogram(256) ;
     vector<int> eq_histogram(256);
     vector<int> cdf(256);
 
-//    cout << image_src.rows << "\t" << image_src.cols << endl;
-
     //creation of histogram
-    for( int i = 0 ; i < image_src.rows; i++)
-        for( int j = 0 ; j < image_src.cols; j++)
+    for( int i = 0 ; i < image_src.rows; i++){
+        for( int j = 0 ; j < image_src.cols; j++){
             histogram [ (int) image_src.at<uchar>(i,j) ]++;
+        }
+    }
 
     int higher = -1;
 
@@ -43,8 +63,9 @@ int main(int argc, char** argv )
 */
 
     //Drawing Histogram
+    myfile <<"Histogram \n";
     for( int i = 0 ; i < histSize; i++) {
-        //cout << " val " << i << "\t hist " << histogram[i] << endl;
+        myfile << " val " << i << "\t hist " << histogram[i] << endl;
         line( histogram_graph, Point( i, 0 ), Point( i, double(histogram[i] / 10) ) , Scalar(0,255,0), 2, 8, 0 );
     }
 
@@ -55,10 +76,13 @@ int main(int argc, char** argv )
     int lower = 1000000;
 
     //calculating cdf
+    myfile <<"\nCDF\n";
     for( int i = 1; i < histogram.size(); i++ ){
         eq_histogram[i] = eq_histogram[i-1] + histogram[i];
+        myfile << " val " << i << "\t = " << eq_histogram[i] << endl;
         if( eq_histogram[i] < lower) lower = eq_histogram[i] ;
     }
+
     for( int i = 1; i < histogram.size(); i++ ){
         cdf[i] = ( eq_histogram[i]*255 ) / eq_histogram[254] ;
         //cout << cdf [i] << endl;
@@ -67,9 +91,11 @@ int main(int argc, char** argv )
     cout << "lower : " << lower << endl;
 
     //calculating eq_histogram
+    myfile <<"\nEQ Table\n";
     for( int i = 0; i < histogram.size(); i++ ){
         eq_histogram[i] = ( (eq_histogram[i] - lower) * 255 )/ ( image_src.rows * image_src.cols - lower);
-        cout << "val " << i << " : " <<  eq_histogram[i] << endl;
+        myfile << " val " << i << "\t = " << cdf[i] << endl;
+        //cout << "val " << i << " : " <<  eq_histogram[i] << endl;
     }
 
 
@@ -80,15 +106,24 @@ int main(int argc, char** argv )
     }
 
     //Updating eq image
-    for( int i =  0; i < image_src.rows; i++)
-        for( int j = 0; j < image_src.cols; j++){
+    for( int i =  0; i < image_height; i++){
+        for( int j = 0; j < image_width; j++){
             //cout << (int) image_dest.at<uchar>(i,j) <<"//" <<  eq_histogram[ (int) image_dest.at<uchar>(i,j) ] << "\t";
             image_dest.at<uchar>(i,j) = (uchar) eq_histogram[ (int) image_dest.at<uchar>(i,j) ];
             //cout << (int) image_dest.at<uchar>(i,j) << endl;
         }
+    }
 
     cout << endl;
-
+/*
+    for( int i =  0; i < image_src.rows; i++){
+        for( int j = 0; j < image_src.cols; j++){
+ //           cout << (int) image_dest.at<uchar>(i,j) <<"//" <<  eq_histogram[ (int) image_dest.at<uchar>(i,j) ] << "\t";
+            image_dest.at<uchar>(i,j) = (uchar) eq_histogram[ (int) image_dest.at<uchar>(i,j) ];
+        }
+    }
+*/
+    myfile.close();
 
     //Display
     namedWindow("Source ", CV_WINDOW_AUTOSIZE );
