ddd89c5fdcfb7b3295c52b4fb92f256ac2f60b00
jecs89 <jecs89@gmail.com>
2014-11-05 21:21:08 -0500

    Dominat Color Cuda

--- test_dominantcolor.cu
+++ test_dominantcolor.cu
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <math.h>
 #include <iomanip>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+struct RGB{
+	unsigned char R,G,B;
+};
 
 __global__ void add(int *a, int *b, int *c, int n){
 	int index = threadIdx.x + blockIdx.x*blockDim.x;
@@ -18,9 +26,63 @@ __global__ void print(int *a){
 #define N (1024)
 #define M (1000000)
 
+int space = 4;
+
 int main(void){
 
-	time_t timer = time(0);	
+	ifstream points	("points.data");
+	ifstream image  ("image.data");
+
+	if( !points.is_open() || !image.is_open()){
+		cout << "Problems opening file \n";
+	}
+
+	vector<vector<RGB>> image_src;
+	vector<pair<int,int>> v_points;
+
+	string line1, line2, sR, sG, sB;
+	int R=0, G=0, B=0;
+
+	getline( image, line1 );
+
+	line2 = line1;
+
+	line1 = line1.substr(0, space);
+	line2 = line2.substr(space);
+
+	if ( ! (istringstream(line1) >> B) ) B = 0;
+	if ( ! (istringstream(line2) >> G) ) G = 0;
+
+	image_src.resize( B );
+
+	for( int i = 0 ; i < B ; ++i)
+		image_src[i].resize(G);
+
+    time_t timer = time(0); 
+
+	while( getline( image, line1 ) ){
+		line2 = line1;
+		for( int i = 0 ; i < image_src.size(); ++i){
+			for( int j = 0 ; j < image_src[0].size(); ++j){
+
+				//line1 = line1.substr( 0, space*3 );
+				sB = line1.substr(0,space);
+				sB = line1.substr(space,space);
+				sB = line1.substr(space*2,space);
+
+				if ( ! (istringstream(sB) >> image_src[i][j].B) ) image_src[i][j].B = 0;
+				if ( ! (istringstream(sG) >> image_src[i][j].G) ) image_src[i][j].G = 0;
+				if ( ! (istringstream(sR) >> image_src[i][j].R) ) image_src[i][j].R = 0;
+
+				line1.substr( space*3 );
+			}
+		}
+	}
+
+	time_t timer2 = time(0);
+    cout <<"Tiempo total: " << difftime(timer2, timer) << endl;
+
+/*	time_t timer = time(0);
 
 	int *a,*b,*c;					// host copies of a,b,c
 	int *d_a, *d_b, *d_c;		// device copies of a,b,c
@@ -68,7 +130,7 @@ int main(void){
 		std::cout << setw(6) << c[i];
 
 	std::cout << std::endl;
-*/
+
 	// Cleanup
 	free(a);
 	free(b);
@@ -80,5 +142,5 @@ int main(void){
 	time_t timer2 = time(0);
 	cout <<"Tiempo total: " << difftime(timer2, timer) << endl;
 
-	return 0;
+*/	return 0;
 }
